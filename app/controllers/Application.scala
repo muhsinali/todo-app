fb2297d44a1bb656b6dbb3479763e53211b52017
muhsinali <muhsinali10@gmail.com>
2017-02-06 21:47:18 +0000

    Add TaskDAO and main view

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,19 +1,40 @@
 package controllers
 
+import javax.inject.Inject
+
+import models.TaskData
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, Controller}
+import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
 
 import scala.concurrent.Future
 
 /**
   * Created by Muhsin Ali on 06/02/2017.
   */
-class Application extends Controller {
+class Application @Inject()(val reactiveMongoApi: ReactiveMongoApi, val messagesApi: MessagesApi) extends Controller with MongoController
+    with ReactiveMongoComponents with I18nSupport {
+
   import scala.concurrent.ExecutionContext.Implicits.global
+  val taskDAO = new TaskDAO(reactiveMongoApi)
 
   def index() = Action.async {implicit request =>
-    Future(Ok("This is the homepage"))
+    Future(Ok(views.html.main(TaskDAO.createTaskForm)))
   }
 
+
+  def createTask() = Action.async {implicit request =>
+    def failure(badForm: Form[TaskData]) = {
+      Future(BadRequest(badForm.errorsAsJson))
+    }
+    def success(taskData: TaskData) = {
+      Future(Ok("Crated task"))
+    }
+
+    val form = TaskDAO.createTaskForm.bindFromRequest()
+    form.fold(failure, success)
+  }
 
 
 }
--- app/controllers/TaskDAO.scala
+++ app/controllers/TaskDAO.scala
@@ -1,8 +1,34 @@
 package controllers
 
+import javax.inject.Inject
+
+import models.TaskData
+import play.api.data.Forms._
+import play.api.data._
+import play.api.mvc.Controller
+import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
+import reactivemongo.play.json.collection.JSONCollection
+
+import scala.concurrent.{ExecutionContext, Future}
+
 /**
   * Created by Muhsin Ali on 06/02/2017.
   */
-class TaskDAO {
+class TaskDAO @Inject()(val reactiveMongoApi: ReactiveMongoApi)(implicit ec: ExecutionContext) extends Controller
+    with MongoController with ReactiveMongoComponents {
 
+
+
+  def tasksFuture: Future[JSONCollection] = database.map(_.collection[JSONCollection]("tasks"))
+}
+
+
+object TaskDAO {
+  val createTaskForm = Form(
+    mapping(
+      "id" -> optional(number),
+      "dueDate" -> date,
+      "description" -> nonEmptyText
+    )(TaskData.apply)(TaskData.unapply)
+  )
 }
