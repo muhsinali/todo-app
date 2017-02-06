41166f2aeecb9fdb0636a26b8eb7cdd9519654ac
Muhsin Ali <muhsinali@users.noreply.github.com>
2017-02-06 21:21:34 +0000

    Init commit

---
+++ app/models/Task.scala
@@ -0,0 +1,18 @@
+package models
+
+import java.util.Date
+
+import play.api.libs.json.Json
+
+/**
+  * Created by Muhsin Ali on 06/02/2017.
+  */
+case class Task(date: Date, description: String)
+
+case class TaskData(id: Option[Int], date: Date, description: String)
+
+
+
+object Task {
+  implicit val formatter = Json.format[Task]
+}
