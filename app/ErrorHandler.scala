bee6d0c21385b926667497ee5cf048b3f5ae79c1
Muhsin Ali <muhsinali@users.noreply.github.com>
2017-02-07 23:33:17 +0000

    Create custom error handler class

---
+++ app/ErrorHandler.scala
@@ -0,0 +1,25 @@
+import javax.inject.{Inject, Singleton}
+
+import play.api.http.HttpErrorHandler
+import play.api.mvc.Results._
+import play.api.mvc.{RequestHeader, Result}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+/**
+  * Used to provide custom implementations for handling client errors.
+  */
+@Singleton
+class ErrorHandler @Inject()(implicit ec: ExecutionContext) extends HttpErrorHandler {
+  override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
+    if(statusCode == play.api.http.Status.NOT_FOUND) {
+      return Future(NotFound("Oops, getting a 404 here!"))
+    }
+
+    Future(Status(statusCode)("A client error occurred: " + message))
+  }
+
+  override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
+    Future(InternalServerError("A server error occurred: " + exception.getMessage))
+  }
+}
