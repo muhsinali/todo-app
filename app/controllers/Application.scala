056b200b0fefc1905b38ca93c9e387b14e212989
Muhsin Ali <muhsinali@users.noreply.github.com>
2017-02-14 01:07:55 +0000

    #4: Add a task at startup

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,14 +1,19 @@
 package controllers
 
+import java.io.File
+import java.text.SimpleDateFormat
 import javax.inject.Inject
 
 import models.TaskData
+import play.api.Logger
 import play.api.data.Form
 import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{Action, Controller}
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
 
 import scala.concurrent.Future
+import scala.io.Source
 
 /**
   * Created by Muhsin Ali on 06/02/2017.
@@ -18,6 +23,28 @@ class Application @Inject()(val reactiveMongoApi: ReactiveMongoApi, val messages
 
   import scala.concurrent.ExecutionContext.Implicits.global
   val taskDAO = new TaskDAO(reactiveMongoApi)
+
+  onStartup()
+
+
+  def onStartup(): Unit = {
+    def getJsonProperty(jsValue: JsValue, field: String) = (jsValue \ field).as[String]
+
+    val file = new File("public/exampleTask.json")
+    assert(file.exists() && file.isFile && file.canRead)
+
+    val source = Source.fromFile(file)
+    val parsedJson = Json.parse(source.mkString)
+    val title = getJsonProperty(parsedJson, "title")
+    val description = getJsonProperty(parsedJson, "description")
+    val dueDate = getJsonProperty(parsedJson, "dueDate")
+    source.close()
+
+    taskDAO.create(title, description, new SimpleDateFormat("yyyy-MM-dd").parse(dueDate)).onFailure{
+      case throwable => Logger.error("Failed to create task object", throwable)
+    }
+  }
+
 
   def index() = Action.async {implicit request =>
     taskDAO.getAllTasks.map(tasks => Ok(views.html.main(tasks, TaskDAO.createTaskForm)))
