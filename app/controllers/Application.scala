c6c37789a635dc3dab9692b1fec545101c128fc1
Muhsin Ali <muhsinali@users.noreply.github.com>
2017-02-23 23:46:20 +0000

    Add edit functionality

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,28 +8,16 @@ import play.api.data.Form
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, Controller}
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
+import reactivemongo.api.commands.{UpdateWriteResult, WriteResult}
 
 import scala.concurrent.{ExecutionContext, Future}
 
-/**
-  * Created by Muhsin Ali on 06/02/2017.
-  */
+
 class Application @Inject()(val reactiveMongoApi: ReactiveMongoApi, val messagesApi: MessagesApi)
                            (implicit ec: ExecutionContext) extends Controller with MongoController
     with ReactiveMongoComponents with I18nSupport {
 
   val taskDAO = new TaskDAO(reactiveMongoApi)
-
-  def createTask() = Action.async {implicit request =>
-    def failure(formWithErrors: Form[TaskData]) = {
-      taskDAO.all.map(tasks => BadRequest(views.html.main(tasks, formWithErrors)))
-    }
-    def success(taskData: TaskData) = {
-      taskDAO.create(taskData)
-      Future(Redirect(routes.Application.index()).flashing("success" -> "Created task"))
-    }
-    TaskDAO.createTaskForm.bindFromRequest().fold(failure, success)
-  }
 
   // TODO get the flash scope to show up
   def deleteTask(id: Int) = Action.async {implicit request =>
@@ -42,7 +30,8 @@ class Application @Inject()(val reactiveMongoApi: ReactiveMongoApi, val messages
   def editTask(id: Int) = Action.async {implicit request =>
     taskDAO.findById(id).flatMap {
       case Some(task) =>
-        val taskData: TaskData = TaskData(Some(task.id), task.title, task.description, new SimpleDateFormat("dd-MM-yyyy").parse(task.dueDate))
+        val taskData: TaskData = TaskData(Some(task.id), task.title, task.description,
+          new SimpleDateFormat("dd-MM-yyyy").parse(task.dueDate))
         taskDAO.all.map(tasks => Ok(views.html.main(tasks, TaskDAO.createTaskForm.fill(taskData))));
       case None => Future(Redirect(routes.Application.index()).flashing("error" -> s"Could not edit task with id $id"))
     }
@@ -52,6 +41,30 @@ class Application @Inject()(val reactiveMongoApi: ReactiveMongoApi, val messages
 
   def index() = Action.async {implicit request =>
     taskDAO.all.map(tasks => Ok(views.html.main(tasks, TaskDAO.createTaskForm)))
+  }
+
+  def uploadTask() = Action.async { implicit request =>
+    def failure(formWithErrors: Form[TaskData]) = {
+      taskDAO.all.map(tasks => BadRequest(views.html.main(tasks, formWithErrors)))
+    }
+
+    def success(taskData: TaskData) = {
+      val writeResult = taskData.id match {
+        case Some(id) => taskDAO.update(taskData)
+        case None => taskDAO.create(taskData)
+      }
+      writeResult.map {
+        case w: UpdateWriteResult =>
+          val flashMessage = if(w.ok) "success" -> "Updated task" else "error" -> "Could not update task"
+          Redirect(routes.Application.index()).flashing(flashMessage)
+        case w: WriteResult =>
+          val flashMessage = if(w.ok) "success" -> "Created task" else "error" -> "Could not create task"
+          Redirect(routes.Application.index()).flashing(flashMessage)
+        case _ => throw new IllegalArgumentException
+      }
+    }
+
+    TaskDAO.createTaskForm.bindFromRequest().fold(failure, success)
   }
 
 
